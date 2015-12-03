61f150ebfb800fff068705a3ec4bfba5fadb8d11
Shu Zhou <shuzhou@usc.edu>
2015-12-03 10:09:44 -0800

    testing heroku failing error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,12 +12,12 @@ class StaticPagesController < ApplicationController
   	str = "Daily High-resolution Blended Analyses for sea surface temperature"
   	cmd = './scholar.py -p "#{str}" --list-citations'
   	data_hash = `#{cmd}`
-  	if data_hash.nil? || data_hash.empty?
-  		file = File.read(Rails.root.join("app/assets/quikscat.json"))
-  		data_hash = JSON.parse(file) 		
-  	else
-  		data_hash = JSON.parse(data_hash)
-  	end
+  	#if data_hash.nil? || data_hash.empty?
+  	file = File.read(Rails.root.join("app/assets/quikscat.json"))
+  	data_hash = JSON.parse(file) 		
+  	#else
+  	#	data_hash = JSON.parse(data_hash)
+  	#end
   	render json:JSON.pretty_generate(data_hash)
   end
 
