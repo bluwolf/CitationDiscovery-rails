fd16f48ad5f69a7cc200cdbbc2ad92672e2740ff
Xiaohan Chen <xiaohanc@usc.edu>
2015-12-03 11:41:10 -0800

    trying

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,13 +14,13 @@ class StaticPagesController < ApplicationController
   	str = "Daily High-resolution Blended Analyses for sea surface temperature"
   	cmd = './scholar.py -p "Daily High-resolution Blended Analyses for sea surface temperature" --list-citations'
   	data_hash = `#{cmd}`
-  	#puts "Shu", data_hash
-  	#if data_hash == nil
-  		#file = File.read(Rails.root.join("app/assets/ccmp.json"))
-  		#data_hash = JSON.parse(file)
-  	#else
-  	data_hash = JSON.parse(data_hash)
-  	#end
+  	#puts "shu", data_hash
+  	# if data_hash
+    #   data_hash = JSON.parse(data_hash)
+  	# else
+      file = File.read(Rails.root.join("app/assets/ccmp.json"))
+      data_hash = JSON.parse(file)
+  	# end
   	render json:JSON.pretty_generate(data_hash)
   end
 
