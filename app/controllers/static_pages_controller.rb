eec61a80d143408ecbed8e37c44fad3ff94c162e
Xiaohan Chen <xiaohanc@usc.edu>
2015-12-02 23:29:32 -0800

    merging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,15 +12,15 @@ class StaticPagesController < ApplicationController
   	#file = File.read(Rails.root.join("app/assets/ccmp.json"))
   	#data_hash = JSON.parse(file)
   	str = "Daily High-resolution Blended Analyses for sea surface temperature"
-  	cmd = './scholar.py -p #{str} --list-citations'
+  	cmd = './scholar.py -p "Daily High-resolution Blended Analyses for sea surface temperature" --list-citations'
   	data_hash = `#{cmd}`
-  	puts "Shu", data_hash
-  	if data_hash != nil
-  		file = File.read(Rails.root.join("app/assets/ccmp.json"))
-  		data_hash = JSON.parse(file)
-  	else
-  		data_hash = JSON.parse(data_hash)
-  	end
+  	#puts "Shu", data_hash
+  	#if data_hash == nil
+  		#file = File.read(Rails.root.join("app/assets/ccmp.json"))
+  		#data_hash = JSON.parse(file)
+  	#else
+  	data_hash = JSON.parse(data_hash)
+  	#end
   	render json:JSON.pretty_generate(data_hash)
   end
 
