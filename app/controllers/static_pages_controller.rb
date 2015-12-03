0182975944d02b0986e08fe92e32c32f108539f0
Xiaohan Chen <xiaohanc@usc.edu>
2015-12-02 22:55:14 -0800

    merging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,9 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-<<<<<<< HEAD
   def results
   end
 
-  def load
-=======
   def load
   	#file = File.read(Rails.root.join("app/assets/ccmp.json"))
   	#data_hash = JSON.parse(file)
@@ -22,6 +19,5 @@ class StaticPagesController < ApplicationController
   		data_hash = JSON.parse(data_hash)
   	end
   	render json:JSON.pretty_generate(data_hash)
->>>>>>> 63e360cd98f311660d92ca8789e629331c075cb1
-  end
+
 end
