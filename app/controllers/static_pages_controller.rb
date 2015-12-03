63e360cd98f311660d92ca8789e629331c075cb1
Shu Zhou <shuzhou@usc.edu>
2015-12-02 22:38:51 -0800

    add /load to execute shell script

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def load
+  	#file = File.read(Rails.root.join("app/assets/ccmp.json"))
+  	#data_hash = JSON.parse(file)
+  	str = "Daily High-resolution Blended Analyses for sea surface temperature"
+  	cmd = './scholar.py -p #{str} --list-citations'
+  	data_hash = `#{cmd}`
+  	puts "Shu", data_hash
+  	if data_hash != nil
+  		file = File.read(Rails.root.join("app/assets/ccmp.json"))
+  		data_hash = JSON.parse(file)
+  	else
+  		data_hash = JSON.parse(data_hash)
+  	end
+  	render json:JSON.pretty_generate(data_hash)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'welcome' => 'static_pages#home'
-
+  get 'load' => 'static_pages#load'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
